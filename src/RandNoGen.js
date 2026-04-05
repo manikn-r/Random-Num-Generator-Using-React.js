196058eb727cbf8d8000ec4068eabe25f6672a4b
manikandan.ramachandran-at-178736008566 <manikandan.ramachandran@exterro.com>
2026-04-05 10:54:58 +0530

    initial commit

---
+++ src/RandNoGen.js
@@ -0,0 +1,19 @@
+import { useState } from "react";
+
+function RandNoGen(){
+    let [randNo,updateNo] = useState(0);
+        let genNum = ()=>{
+updateNo(Math.floor((Math.random()*100)+1));
+    }
+return(
+    <>
+    <div className="rand-container">
+    <div className="title"><h2>Random Number Generator ( 1 to 100 )</h2></div>
+    <div className="displayNo">{randNo == 0 ? "No number generated yet": randNo}</div>
+    <br></br>
+    <button onClick={genNum}>Generate Random Number</button>
+    </div>
+    </>
+)
+}
+export default RandNoGen;
