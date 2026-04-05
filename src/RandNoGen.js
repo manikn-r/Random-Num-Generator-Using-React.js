a06d02229321c58db409f196d4263fa8f10452c0
manikandan.ramachandran-at-178736008566 <manikandan.ramachandran@exterro.com>
2026-04-05 10:57:52 +0530

    update file

--- src/RandNoGen.js
+++ src/RandNoGen.js
@@ -9,7 +9,7 @@ return(
     <>
     <div className="rand-container">
     <div className="title"><h2>Random Number Generator ( 1 to 100 )</h2></div>
-    <div className="displayNo">{randNo == 0 ? "No number generated yet": randNo}</div>
+    <div className="displayNo">{randNo === 0 ? "No number generated yet": randNo}</div>
     <br></br>
     <button onClick={genNum}>Generate Random Number</button>
     </div>
